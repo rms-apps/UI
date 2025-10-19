770a94fa7193d6ab328a1ef346164e29adcff164
Rishi Mishra <service.rmapps@gmail.com>
2025-10-19 05:31:08 +0530

    fix: previous

--- packages/Utils/src/customHooks/useAppFonts.ts
+++ packages/Utils/src/customHooks/useAppFonts.ts
@@ -1,19 +1,17 @@
-// /* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-require-imports */
 
-// import { useFonts } from 'expo-font';
+import { useFonts } from 'expo-font';
 
-// export const useAppFonts = () => {
-//   return useFonts({
-//     NotoSansExtraBold: require('../assets/fonts/NotoSans-ExtraBold.ttf'),
-//     NotoSansBold: require('../assets/fonts/NotoSans-Bold.ttf'),
-//     NotoSansSemiBold: require('../assets/fonts/NotoSans-SemiBold.ttf'),
-//     NotoSansMedium: require('../assets/fonts/NotoSans-Medium.ttf'),
-//     NotoSansRegular: require('../assets/fonts/NotoSans-Regular.ttf'),
-//     NotoSansThin: require('../assets/fonts/NotoSans-Thin.ttf'),
-//     NotoSansLight: require('../assets/fonts/NotoSans-Light.ttf'),
-//     NotoSansCondensedSemiBold: require('../assets/fonts/NotoSans_Condensed-SemiBold.ttf'),
-//     NotoSansCondensedRegular: require('../assets/fonts/NotoSans_Condensed-Regular.ttf'),
-//   });
-// };
-
-export {};
+export const useAppFonts = () => {
+  return useFonts({
+    NotoSansExtraBold: require('../assets/fonts/NotoSans-ExtraBold.ttf'),
+    NotoSansBold: require('../assets/fonts/NotoSans-Bold.ttf'),
+    NotoSansSemiBold: require('../assets/fonts/NotoSans-SemiBold.ttf'),
+    NotoSansMedium: require('../assets/fonts/NotoSans-Medium.ttf'),
+    NotoSansRegular: require('../assets/fonts/NotoSans-Regular.ttf'),
+    NotoSansThin: require('../assets/fonts/NotoSans-Thin.ttf'),
+    NotoSansLight: require('../assets/fonts/NotoSans-Light.ttf'),
+    NotoSansCondensedSemiBold: require('../assets/fonts/NotoSans_Condensed-SemiBold.ttf'),
+    NotoSansCondensedRegular: require('../assets/fonts/NotoSans_Condensed-Regular.ttf'),
+  });
+};
